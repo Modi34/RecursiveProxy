9d6b20a7564fbac172ff938b279c6b4aa03e2520
Oleg Buylov <modi34@yandex.ru>
2021-07-09 00:05:05 +0300

    refactoring

--- rp.js
+++ rp.js
@@ -2,23 +2,19 @@ class RecursiveProxy{
 	isProxy = true
 	subs = {}
 	handler = {
-		get: (obj, prop) => {
-			if(this[prop]){return this[prop]}
-
-			return obj[prop]
-		},
+		get: (obj, prop) => this[prop] || obj[prop],
 		set: (obj, prop, value) => {
 			let old = obj[prop];
 			obj[prop] =
 				value + '' == {} & !value.isProxy ?
 					new RecursiveProxy(value) : value;
-			this.subs[prop] && this.subs[prop](old, obj[prop])
+			this.subs[prop] &&
+				this.subs[prop](old, obj[prop])
 		}
 	}
-	constructor(obj){
-		for(let key in obj){
-			this.handler.set(this, key, obj[key])
-		}
+	constructor(obj = {}){
+		for(let key in obj)
+			this.handler.set(this, key, obj[key]);
 
 		return new Proxy(this, this.handler)
 	}
