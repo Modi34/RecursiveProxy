2021ff00f849a56569dcf1c8e44d6ffb790d9ea1
Oleg Buylov <modi34@yandex.ru>
2021-07-08 23:57:48 +0300

    Ability to subscirbe

--- rp.js
+++ rp.js
@@ -1,22 +1,28 @@
 class RecursiveProxy{
-	isRecursiveProxy = true
+	isProxy = true
+	subs = {}
 	handler = {
-		get: (obj, prop, proxy) => {
+		get: (obj, prop) => {
 			if(this[prop]){return this[prop]}
 
 			return obj[prop]
 		},
-		set(obj, prop, value){
+		set: (obj, prop, value) => {
+			let old = obj[prop];
 			obj[prop] =
-				value + '' == {} & !value.isRecursiveProxy ?
-					new RecursiveProxy(value, obj, prop) : value;
+				value + '' == {} & !value.isProxy ?
+					new RecursiveProxy(value) : value;
+			this.subs[prop] && this.subs[prop](old, obj[prop])
 		}
 	}
-	constructor(obj, parent = false, key = ''){
+	constructor(obj){
 		for(let key in obj){
 			this.handler.set(this, key, obj[key])
 		}
 
 		return new Proxy(this, this.handler)
+	}
+	subscribe(key, callback){
+		this.subs[key] = callback
 	}
 }
